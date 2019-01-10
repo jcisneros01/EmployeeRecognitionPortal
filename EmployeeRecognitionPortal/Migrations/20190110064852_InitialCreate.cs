545fcf1e2e53bfe5bda88c9d119aef3b6ca8407e
Jose Cisneros <cisnejos@oregonstate.edu>
2019-01-10 01:21:52 -0700

    create user migration

---
+++ EmployeeRecognitionPortal/Migrations/20190110064852_InitialCreate.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EmployeeRecognitionPortal.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Email = table.Column<string>(nullable: true),
+                    Name = table.Column<string>(nullable: true),
+                    DateCreated = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.UserId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
---
+++ EmployeeRecognitionPortal/Models/User.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace EmployeeRecognitionPortal.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string Email { get; set; }
+        public string Name { get; set; }
+        public DateTime DateCreated { get; set; }
+    }
+}
