327cd622e7ff10f80e8b7182f16a28d37e202ffa
Jose Cisneros <cisnejos@oregonstate.edu>
2019-01-20 22:43:14 -0700

    added user response/request models

---
+++ EmployeeRecognitionPortal/Models/Request/UserRequest.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeRecognitionPortal.Models.Request
+{
+    public class UserRequest
+    {
+        public string Email { get; set; }
+        
+        public string Password { get; set; }
+        
+        public string Name { get; set; }
+        
+        public byte[] Signature { get; set; }
+    }
+}
--- EmployeeRecognitionPortal/Models/Response/UserResponse.cs
+++ EmployeeRecognitionPortal/Models/Response/UserResponse.cs
@@ -1,28 +1,17 @@
-using System;
 using System.ComponentModel.DataAnnotations;
 
-namespace EmployeeRecognitionPortal.Models
+namespace EmployeeRecognitionPortal.Models.Response
 {
-    public class User
+    public class UserResponse
     {
-        [Key]
         public int Id { get; set; }
         
-        [Required]
         public string Email { get; set; }
         
-        [Required]
         public string Password { get; set; }
         
-        [Required]
         public string Name { get; set; }
         
-        [Required]
         public byte[] Signature { get; set; }
-        
-        [Required]
-        public DateTime DateCreated { get; set; }
-
-        public bool? IsDeleted { get; set; }
     }
 }
