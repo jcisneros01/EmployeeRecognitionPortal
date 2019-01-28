3c5fe7b20b42ab6f284b70d985afb204d5ab1225
Zach <zdmitchell@gmail.com>
2019-01-28 14:55:34 -0800

    Created Award Classes and accompanying LaTex templates

---
+++ EmployeeRecognitionPortal/Entities/AwardMonth.cs
@@ -0,0 +1,22 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeRecognitionPortal.Models
+{
+    public class AwardMonth
+    {
+      [Required]
+      public string EmployeeName {get; set;}
+
+      [Required]
+      public string EmployeeEmail {get; set;}
+
+      [Required]
+      public DateTime DateAwarded {get; set;}
+
+      [Required]
+      public User AwardCreator {get; set; }
+
+      public string LaTexFile { get; set; }
+    }
+}
