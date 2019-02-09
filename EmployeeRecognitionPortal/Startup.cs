790ffd34d063994d2b3744a921411154ab191739
Jose Cisneros <cisnejos@oregonstate.edu>
2019-02-09 14:27:04 -0700

    config swagger

--- EmployeeRecognitionPortal/Startup.cs
+++ EmployeeRecognitionPortal/Startup.cs
@@ -14,6 +14,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.IdentityModel.Tokens;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace EmployeeRecognitionPortal
 {
@@ -71,6 +72,12 @@ namespace EmployeeRecognitionPortal
                 });
             
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "Employee Recognition Portal API", Version = "v1" });
+            });
 
             //todo: move connection string to config file
             // Connect to database
@@ -94,6 +101,16 @@ namespace EmployeeRecognitionPortal
             app.UseSpaStaticFiles();
             app.UseCors("AllowAllOrigins");
             app.UseAuthentication();
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Employee Recognition Portal API");
+            });
+
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
