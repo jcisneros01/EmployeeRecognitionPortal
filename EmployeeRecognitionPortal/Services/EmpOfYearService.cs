24d0656bd7e3faac3626d04f2fe2baf25c353b57
zachmitch <zdmitchell@gmail.com>
2019-02-15 16:49:02 -0800

    Added award generation functions, commented them out so app works

--- EmployeeRecognitionPortal/Services/EmpOfYearService.cs
+++ EmployeeRecognitionPortal/Services/EmpOfYearService.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using AutoMapper;
 using EmployeeRecognitionPortal.Exceptions;
@@ -6,6 +8,11 @@ using EmployeeRecognitionPortal.Models;
 using EmployeeRecognitionPortal.Models.Request;
 using EmployeeRecognitionPortal.Models.Response;
 using Microsoft.EntityFrameworkCore;
+using System.IO;
+using System.Net;
+using System.Net.Mail;
+using System.Net.Mime;
+using System.Text;
 
 namespace EmployeeRecognitionPortal.Services
 {
@@ -24,11 +31,17 @@ namespace EmployeeRecognitionPortal.Services
         {
            var empOfYear = _mapper.Map<EmpOfYearRequest, EmpOfYear>(eoy);
             
+           //string pdfFilePath = 
+               
            GenerateLatexFile(eoy.AwardCreatorId, empOfYear);
             
            _context.EmpOfYears.Add(empOfYear);
            _context.SaveChanges();
-
+           
+           //EmailAward(pdfFilePath, eoy.AwardCreatorId, empOfYear);
+           
+           //CleanupLatex(pdfFilePath);
+           
            return _mapper.Map<EmpOfYear, EmpOfYearResponse>(empOfYear);
         }
         
@@ -44,7 +57,134 @@ namespace EmployeeRecognitionPortal.Services
 
             empOfYear.AwardCreator = awardCreator;
             empOfYear.CreateLaTex();
+            
+            //return BuildLatexFile(empOfYear, Convert.ToBase64String(awardCreator.Signature));
+
         }
+        
+//         private string BuildLatexFile(EmpOfYear empOfYear, string sig)
+//        {
+//            // Make temporary directory to hold necessary files
+//            Random rnd = new Random();
+//            int tempNum = rnd.Next(10000);
+//            string filePath = "empTemp" + tempNum + "/";
+//
+//            DirectoryInfo di; // directory
+//            FileStream fs;   // actual LaTex file
+//
+//          
+//            if (Directory.Exists(filePath))
+//            {
+//                Directory.Delete(filePath);
+//            }
+//
+//            //Create directory and file
+//            di = Directory.CreateDirectory(filePath);
+//            fs = File.Create(di.ToString() + "latex" + tempNum.ToString() + ".tex");
+//
+//            Byte[] lt = new UTF8Encoding(true).GetBytes(empOfYear.LaTexFile);
+//            fs.Write(lt, 0, lt.Length);
+//            fs.Close();
+//            
+//            //Convert 64bit encoded signature to png
+//            Base64ToImage(sig, filePath + "sig.png");
+//
+//            // Copy award background
+//            File.Copy(Directory.GetCurrentDirectory() + "/Entities/AwardTemplates/certBorder.jpg",
+//                di + "/certBorder.jpg");
+//                
+//            try
+//            {
+//                // Run latexmk
+//                var proc = new Process()
+//                {
+//                    StartInfo = new ProcessStartInfo
+//                    {
+//                        WorkingDirectory = di.Name,
+//                        FileName = "latexmk",
+//                        Arguments = $"-pdf -f " + "latex" + tempNum.ToString() + ".tex",
+//                        RedirectStandardOutput = false,
+//                        UseShellExecute = false,
+//                        CreateNoWindow = true
+//                    }
+//                };
+//                proc.Start();
+//                proc.WaitForExit();
+//
+//            }
+//            catch (Exception ex)
+//            {
+//                Console.WriteLine(ex.ToString());
+//            }
+//                   
+//            //Return pdf file
+//            return (@di.FullName.ToString() + "latex" +  tempNum.ToString() + ".pdf");
+//        }
+//        
+//
+//        private void Base64ToImage(string b64, string path)
+//        {
+//            File.WriteAllBytes(path, Convert.FromBase64String(b64));
+//        }
+//        
+//
+//        private void EmailAward(string filePath,int awardCreatorId, EmpOfYear empOfYear)
+//        {
+//            
+//            var awardCreator = _context.Users
+//                .Include(x => x.AwardCreator)
+//                .FirstOrDefault(x => x.Id == awardCreatorId)?.AwardCreator;
+//            
+//            //Create a new email object
+//            var msg = new MailMessage(
+//                awardCreator.User.Email,
+//                empOfYear.EmployeeEmail,
+//                "WOW!   Employee of the Year - Congratulations!",
+//                "Hi " + empOfYear.EmployeeName + ",\n\nThank you for all of your hard work this YEAR!  See your attached award.\n\nBest,\n" + awardCreator.Name
+//            );
+//            
+//            // Attach pdf for award
+//            Console.WriteLine("SENDING IN EMAIL: " + filePath);
+//            var pdf = new Attachment(filePath, MediaTypeNames.Application.Pdf);
+//            msg.Attachments.Add(pdf);
+//            
+//            //Send
+//            var client = new SmtpClient
+//            {
+//                Host = "smtp.gmail.com",
+//                Port = 587,
+//                EnableSsl = true,
+//                DeliveryMethod = SmtpDeliveryMethod.Network,
+//                Credentials = new NetworkCredential("employeerecognitionapp@gmail.com", "osuBeavers19"),
+//                Timeout = 20000
+//            };
+//
+//            try
+//            {
+//                client.Send(msg);
+//                pdf.Dispose();
+//            }
+//            catch (Exception ex)
+//            {
+//                Console.Write(ex.ToString());
+//            }
+//        }
+//        
+//        private void CleanupLatex(string filePath)
+//        {
+//            //get directory from string
+//            filePath = filePath.Remove(filePath.IndexOf("latex",0, filePath.Length, StringComparison.Ordinal));
+//            var di = new DirectoryInfo(filePath);
+//            
+//            //delete all files within dir
+//            foreach (var f in di.GetFiles())
+//            {
+//                f.Delete();
+//            }
+//            
+//            // delete dir
+//            di.Delete();
+//        }
 
         public List<EmpOfYearResponse> GetEmpOfYears()
         {
